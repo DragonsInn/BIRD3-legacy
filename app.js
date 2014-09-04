3e6708737da52c51166f9228c2ec791c83972c2e
Ingwie Phoenix <ingwie2000@googlemail.com>
2014-09-05 01:05:46 +0200

    Further bug fix. Security sucks.

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ global.log = new (winston.Logger)({
 app.listen(config.http_port, config.host);
 
 // Set up the web stuff.
+require("./lib/request_handler.js")(app);
 require("./lib/security_handler.js")();
 require("./lib/status_worker.js")(redis);
 require("./lib/update_worker.js")();
-require("./lib/request_handler.js")(app);
 require("./lib/websocket_handler.js")(io);
 
 // Default event.

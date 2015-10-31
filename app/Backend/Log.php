0e91706338be39b201db13cd95ddb088f6be6345
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-10-31 21:38:37 +0100

    Logging improved. Retries if Redis is dead.

--- app/Backend/Log.php
+++ app/Backend/Log.php
@@ -1,15 +1,25 @@
 <?php  namespace BIRD3\Backend;
 
 use Predis\Client;
+use Exception;
 
 class Log {
     private static $redis = null;
     public static function getRedis() {
         if(self::$redis==null) {
             self::$redis = new Client;
-            if(self::$redis->isConnected()) {
-                echo "Error: Can not connect to Redis!!";
-                exit(1);
+            self::$redis->connect();
+            $tries = 5; $connected = null;
+            while(!$connected) {
+                try {
+                    $connected = self::$redis->isConnected();
+                } catch(Exception $e) {
+                    $connected = false;
+                }
+                if($tries == 0)
+                    throw new Exception("Could not connect to Redis.");
+                $tries--;
+                sleep(0.5);
             }
         }
         return self::$redis;
@@ -26,6 +36,12 @@ class Log {
             $pmsg = implode(" ",$args);
             echo "[ERROR]: $pmsg\n";
         }
-        return self::getRedis()->publish("BIRD3", $msg);
+        try {
+            return self::getRedis()->publish("BIRD3", $msg);
+        } catch(Exception $e) {
+            // Try again. Sometimes, Redis just goes flop.
+            // Probably has to do with Workerman's forkaholism. o.o
+            return call_user_func_array([__CLASS__, $method], $args);
+        }
     }
 }

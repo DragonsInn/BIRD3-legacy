86b567810842c5016481c212ac573f725a09d6bc
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-11-15 01:47:20 +0100

    MainController updated slightly.

--- app/App/Controllers/MainController.php
+++ app/App/Controllers/MainController.php
@@ -1,11 +1,12 @@
 <?php namespace BIRD3\App\Controllers;
 
 use BIRD3\Foundation\BaseController;
+use View;
 
 class MainController extends BaseController {
     public function getIndex() {
         $this->isIndex = true;
-        return $this->render("site/index");
+        return $this->render("site.index");
     }
     public function getStat() {
         return $this->render("test");

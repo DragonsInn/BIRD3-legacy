384d13ada4f42e23067353b37da3a2f819357522
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-11-27 16:04:34 +0100

    HproseHolder now has a refreshable method.

--- app/Support/HproseHolder.php
+++ app/Support/HproseHolder.php
@@ -4,7 +4,7 @@ use InvalidArgumentException as IVE;
 
 class HproseHolder {
     private $store;
-    public function __construct($data) {
+    public function setContext($data) {
         if(!is_array($data)) {
             throw new IVE("Argument 1 is ".gettype($data).", should be array.");
         }

dfacf5c595b216c100e13cfa159431fc39f71f77
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-12-20 02:54:58 +0100

    Working on views. Created Docs template.

---
+++ app/Resources/Views/docs/base.php
@@ -0,0 +1,18 @@
+<div id="DocTopics">
+    <!--
+        The topic list goes here.
+
+        For instance:
+
+        <b>Derp</b>
+        <ul>
+            <li><a href="/docs/entry-1" data-pjax>Entry 1</a></li>
+            <li><a href="/docs/entry-2" data-pjax>Entry 2</a></li>
+            <li><a href="/docs/entry-3" data-pjax>Entry 3</a></li>
+        </ul>
+    -->
+</div>
+
+<div id="DocBody">
+    <!-- The actual documentation document goes here. -->
+</div>

3947692c539f0f5daf26930c1659d49f2afe72b5
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-12-20 06:42:11 +0100

    Moved submodules into napa.

--- web_modules/LDT.webpack.js
+++ web_modules/LDT.webpack.js
@@ -1,6 +1,6 @@
 // Quickfix to get LDT to speak CommonJS.
-module.exports = require("exports?TextareaDecorator!./LDT/lib/TextareaDecorator");
-module.exports.Keybinder = require("exports?Keybinder!./LDT/lib/Keybinder");
-module.exports.Parser = require("exports?Parser!./LDT/lib/Parser");
-module.exports.SelectHelper = require("exports?SelectHelper!./LDT/lib/SelectHelper");
-module.exports.css = require("./LDT/lib/TextareaDecorator.css");
+module.exports = require("exports?TextareaDecorator!LDT/lib/TextareaDecorator");
+module.exports.Keybinder = require("exports?Keybinder!LDT/lib/Keybinder");
+module.exports.Parser = require("exports?Parser!LDT/lib/Parser");
+module.exports.SelectHelper = require("exports?SelectHelper!LDT/lib/SelectHelper");
+module.exports.css = require("LDT/lib/TextareaDecorator.css");

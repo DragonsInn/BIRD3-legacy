4eb5b587d488a4859b29c72fe85edaf7567058fd
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-12-23 13:17:44 +0100

    Preparing karma environment

---
+++ test/browser/karma.js
@@ -0,0 +1,53 @@
+module.exports = function(config) {
+    // Script for running the tests
+    var runnerScript = require.resolve("./test-runner.js")
+
+    // Karma config
+    var karmaConfig = {
+        browsers: ['PhantomJS'],
+        frameworks: ['jasmine'],
+        reporters: ['spec'],
+        files: [
+            { pattern: runnerScript }
+        ],
+        preprocessors: (function obtainPreprocessors(){
+            var p = {};
+            p[runnerScript] = ["webpack"];
+            return p;
+        })(),
+        webpack: (function obtainWebPackConfig(){
+            // WebPack
+            var webpack = require("webpack");
+            var root = require("find-root")();
+            var webpackConfig = require(root+"/app/System/Config/webpack");
+
+            // Clean the WebPack Config of UglifyJS, if it's there.
+            for(var i=0; i<webpackConfig.plugins.length; i++) {
+                var plugin = webpackConfig.plugins[i];
+                if(
+                        plugin instanceof webpack.optimize.UglifyJsPlugin
+                        || plugin instanceof webpack.optimize.CommonsChunkPlugin
+                ) {
+                    delete webpackConfig.plugins[i];
+                }
+            }
+
+            // Clear modified plugins
+            var plugins = webpackConfig.plugins;
+            plugins = plugins.filter(function(v){ return v != undefined });
+            webpackConfig.plugins = plugins;
+
+            // Enable watch
+            // webpackConfig.watch = true;
+
+            // The testrunner is the ONLY entry.
+            // It require()s the modules and code it'll need.
+            delete webpackConfig.entry;
+
+            return webpackConfig;
+        })()
+    };
+
+    // Set it.
+    config.set(karmaConfig);
+};
---
+++ test/browser/tests/calc.test.js
@@ -0,0 +1,5 @@
+describe("Calc", function(){
+    it("returns 2", function(){
+        expect(2).toBe(2);
+    });
+});

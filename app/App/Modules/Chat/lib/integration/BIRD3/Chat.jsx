6fe6ae87f132b2e7bd3620ceec740dd6eac50042
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-12-27 01:44:11 +0100

    Started to prototype the BIRD3 integrity.

---
+++ app/App/Modules/Chat/lib/integration/BIRD3/Chat.jsx
@@ -0,0 +1,11 @@
+// AjaxChat
+import AJAXChat from "export?ajaxChat!../../../js/chat";
+import ChatConfig from "export?ajaxChatConfig!../../../js/config";
+import ChatLanguage from "export?ajaxChatLang!../../../js/lang/en";
+
+// libs
+// import howler from "howler";
+// import BIRD3Editor from "BIRD3/Extensions/Editor";
+
+// Utilities
+import _ from "microdash";

f27e94617a55b25f0f901dba75935d8c1bf741e0
Ingwie Phoenix <ingwie2000@googlemail.com>
2016-02-07 01:05:20 +0100

    Footer script now resides in Frontend design.

--- app/Frontend/Design/footer.js
+++ app/Frontend/Design/footer.js
@@ -1,17 +1,26 @@
 import oo from "o.o";
+
 export default function makeFooter() {
     var fullHeight = 0
         + oo("#MainPage").height()
         + oo("footer").height();
     if(window.innerHeight < fullHeight) return oo('#MainPage').css('margin-bottom',"auto");
     if(oo("body").height() > oo("#MainPage").height()) {
-        oo('#MainPage').css('margin-bottom',(
-            window.innerHeight
-            - oo('#MainPage').height()
-            - oo('footer').height()
-        )+"px");
+        oo("footer").css({
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0
+        });
     } else {
-        oo('#MainPage').css('margin-bottom',"auto");
+        oo("footer").css({
+            position: "relative"
+        });
     }
 }
+
+// React on resize
 window.addEventListener("resize", makeFooter);
+
+// on DOM ready...
+oo(makeFooter);

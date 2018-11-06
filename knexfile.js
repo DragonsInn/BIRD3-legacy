49d3f05f1b9fb3703577402cc6651e41d215b026
Ingwie Phoenix <ingwie2000@googlemail.com>
2018-11-06 15:21:35 +0100

    Working on DB migrations and ORM.

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      // @todo
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
